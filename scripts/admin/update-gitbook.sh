0044fbef67b4bd7f36021d343c566cc1f5328d02
jgvictores <jcgvicto@gmail.com>
2019-01-02 18:29:26 +0100

    add scripts/admin/update-gitbook.sh

---
+++ scripts/admin/update-gitbook.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Thanks: http://stackoverflow.com/questions/14710257/running-a-cron-job-at-230-am-every-day
+# On how to automate process at 2:30 every day (type "date" to get your server time)
+# crontab -e
+# 30 2 * * * /your/command
+
+echo "Update developer-manual..."
+cd $HOME/developer-manual
+git pull
+echo "Gitbook developer-manual..."
+rm -r _book/
+gitbook build
+cd ..
